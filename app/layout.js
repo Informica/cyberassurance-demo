4dcfdb5489b20943bc97b6ca212026ad560839b7
Informica <hrhacksindia@gmail.com>
2026-01-30 04:09:20 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,18 @@
+"use client";
+
+import "./globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
 // placeholder
